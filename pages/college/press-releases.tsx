d265c7218239464f9c4a235a978e2b364731a328
Amjed Ali K <amjedmgm@gmail.com>
2021-11-28 11:27:41 +0530

    :art: Converted Press and Media page to client side fetching

--- pages/college/press-releases.tsx
+++ pages/college/press-releases.tsx
@@ -1,30 +1,45 @@
 import { NextPage } from "next";
+import useSWR from "swr";
 import PostListItem from "../../components/custom/PostListItem";
 import Container from "../../components/layout/Container";
 import Content from "../../components/layout/Content";
 import Page from "../../components/layout/Page";
 import { H1 } from "../../components/ui/Heading";
+import { fetcher } from "../../server/calls";
 import { NewsMediaType } from "../../server/db";
-import { getAllNews } from "../../server/newsAndMedia";
 
-const CustomPage: NextPage<{ page: NewsMediaType[] }> = ({ page }) => (
-  <Page title="Press Releases">
-    <Container>
-      <H1>Press Releases</H1>
-      <Content>
-        <Content.Left>
-          {page.map((post) => (
-            <PostListItem {...post} key={post.title} />
-          ))}
-        </Content.Left>
-      </Content>
-    </Container>
-  </Page>
-);
+const CustomPage: NextPage = () => {
+  return (
+    <Page title="Press Releases">
+      <Container>
+        <H1>Press Releases</H1>
+        <Content>
+          <Content.Left><NewsList /></Content.Left>
+        </Content>
+      </Container>
+    </Page>
+  );
+};
+
+function NewsList() {
+  const { data, error } = useSWR<NewsMediaType[]>(
+    "/api/news-and-media/all",
+    fetcher
+  );
+
+  if (error) return <div className="text-2xl font-bold text-center text-red-500 my-7">failed to load</div>;
+  if (!data)
+    return (
+      <div className="h-40 max-w-4xl px-10 py-6 my-4 bg-gray-200 border-2 rounded-lg shadow-md cursor-pointer animate-pulse">
+        <div className="flex items-center justify-between"></div>
+      </div>
+    );
+
+  return (
+    <>
+      {data && data.map((post) => <PostListItem {...post} key={post.title} />)}
+    </>
+  );
+}
 
 export default CustomPage;
-
-export async function getStaticProps() {
-  const page = (await getAllNews()) as NewsMediaType[];
-  return { props: { page }, revalidate: 600000 };
-}
