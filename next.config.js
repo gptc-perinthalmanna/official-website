193d5f31cddab9c734b2b4c9f5467fb4dcd98138
Amjed Ali K <amjedmgm@gmail.com>
2021-12-12 22:59:20 +0530

    :rocket:

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
   images: {
     domains: ['i.ibb.co'],
   },
-  env: {
-    DETA_PROJECT_ID: "c062voyt_P9zeR6vfYGNu9Qf1VcuhzdYF33UHaff6",
-  },
+  // env: {
+  //   DETA_PROJECT_ID: "",
+  // },
 }
