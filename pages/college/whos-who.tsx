4145f5bae8a225e75d82df1a430cab15d08904f3
amjed-ali-k <amjedmgm@gmail.com>
2022-07-28 16:48:31 +0400

    Connected api to whos who page

--- pages/college/whos-who.tsx
+++ pages/college/whos-who.tsx
@@ -5,8 +5,8 @@ import Content from "../../components/layout/Content";
 import Page from "../../components/layout/Page";
 import { PageTitle } from "../../components/layout/PageTitle";
 import { UserType } from "../../server/db";
-import { getOther } from "../../server/other";
-import { getUser } from "../../server/users";
+import useSWRImmutable from "swr/immutable";
+import { fetcher } from "../../server/calls";
 
 interface Committee {
   title: string;
@@ -15,77 +15,45 @@ interface Committee {
 
 interface PageType {
   committees: Committee[];
-  staffs: { [key: string]: UserType };
+  staffs: UserType[];
 }
 
-const CustomPage: NextPage<{ page: PageType }> = ({ page }) => {
-  
-  return(
-  <Page title="Who is who">
-    <Container>
-      <PageTitle>Whos Who</PageTitle>
-      <Content>
-        <Content.FullWidth>
-          <div className="mb-5">
-            {page.committees.map((detail) => (
-              <div key={detail.title}>
-                <h2 className="mt-5 text-xl font-bold text-left text-gray-700 lg:text-3xl md:text-2xl">
-                  {detail.title}
-                </h2>
-                <div className="grid grid-cols-2 gap-4 mt-3 lg:grid-cols-3 2xl:grid-cols-4">
-                  {detail.staffs_ids.map((staff) => {
-                    if(!page.staffs[staff.key]) return null;
-                    return(
-                    <UserProfileCard
-                      {...page.staffs[staff.key]}
-                      key={page.staffs[staff.key].key}
-                    />
-                  )})}
+const CustomPage: NextPage = () => {
+  const { data } = useSWRImmutable<PageType>(
+    `/api/pages/others/whos-who`,
+    fetcher
+  );
+
+  return (
+    <Page title="Who is who">
+      <Container>
+        <PageTitle>Whos Who</PageTitle>
+        <Content>
+          <Content.FullWidth>
+            <div className="mb-5">
+              {data?.committees.map((detail) => (
+                <div key={detail.title}>
+                  <h2 className="mt-5 text-xl font-bold text-left text-gray-700 lg:text-3xl md:text-2xl">
+                    {detail.title}
+                  </h2>
+                  <div className="grid grid-cols-2 gap-4 mt-3 lg:grid-cols-3 2xl:grid-cols-4">
+                    {detail.staffs_ids.map((staff) => {
+                      const s = data.staffs.find((s) => s.key === staff.key);
+
+                      if (!s) {
+                        return null;
+                      }
+                      return <UserProfileCard {...s} key={s.key} />;
+                    })}
+                  </div>
                 </div>
-              </div>
-            ))}
-          </div>
-        </Content.FullWidth>
-      </Content>
-    </Container>
-  </Page>
-)};
+              ))}
+            </div>
+          </Content.FullWidth>
+        </Content>
+      </Container>
+    </Page>
+  );
+};
 
 export default CustomPage;
-
-export async function getStaticProps() {
-  const page = (await getOther("who-is-who")) as PageType;
-  let unresolvedpromises: any;
-  let staffs: { [key: string]: UserType } = {};
-  page.committees.forEach((committee) => {
-    unresolvedpromises = committee.staffs_ids.map(async (staff_id) => {
-      if (!("key" in staff_id)) return null;
-      if (staffs[staff_id.key]) return null;
-      try {
-        const user = await getUser(staff_id.key);
-        if (user) {
-          staffs[user?.key] = user;
-          return user;
-        }
-      } catch (error) {
-        console.log(staff_id);
-        console.log(error);
-        return null;
-      }
-
-      return null;
-    });
-  });
-
-  if (unresolvedpromises) await Promise.all(unresolvedpromises);
-
-  return {
-    props: {
-      page: { ...page, staffs },
-    },
-    // Next.js will attempt to re-generate the page:
-    // - When a request comes in
-    // - At most once every 10 seconds
-    revalidate: 600000, // In seconds
-  };
-}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,42 +1,16 @@
-const colors = require('tailwindcss/colors')
-
 module.exports = {
-  content: [
-    './pages/**/*.tsx',
-    './components/**/*.tsx',
-    './common/*.tsx',
-  ],
-  darkMode: 'media', // or 'media' or 'class'
+  content: ["./pages/**/*.tsx", "./components/**/*.tsx", "./common/*.tsx"],
+  darkMode: "media", // or 'media' or 'class'
   theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      white: colors.white,
-      gray: colors.neutral,
-      red: colors.red,
-      blue: colors.sky,
-      yellow: colors.amber,
-      green: colors.green,
-      pink: colors.pink,
-      purple: colors.purple,
-      orange: colors.orange,
-      lime: colors.lime,
-      teal: colors.teal,
-      indigo: colors.indigo,
-      violet: colors.violet,
-      fuchsia: colors.fuchsia,
-      rose: colors.rose,
-    },
     extend: {
       spacing: {
-        
-        '108': '27rem',
-      }
+        108: "27rem",
+      },
     },
   },
-  
+
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
