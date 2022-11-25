5eba726f9289d1b1a6fbcf0e8aace597a84806bb
amjed-ali-k <amjedmgm@gmail.com>
2022-11-25 11:35:46 +0530

    fix: modified photo display

--- components/custom/LargeUserCardWithDetails.tsx
+++ components/custom/LargeUserCardWithDetails.tsx
@@ -22,25 +22,26 @@ function LargeUserCardWithDetails({
   address: string;
   socialLinks: { [key: string]: string };
 }) {
-
   const photo = avatar ? avatar : "/images/avatar-placeholder.png";
   return (
     <div className="flex bg-gray-100 border-2 rounded-lg " {...props}>
       <div className="max-w-full p-3">
         <Image
-          className="rounded-lg"
+          className="rounded-lg object-cover"
           src={photo}
           alt={name}
           height={250}
           width={250}
           placeholder="blur"
-          blurDataURL={process.env.NODE_ENV === "development" ? photo : undefined}
+          blurDataURL={
+            process.env.NODE_ENV === "development" ? photo : undefined
+          }
         />
       </div>
       <div className="p-3">
         <h2 className="text-3xl font-light">{name}</h2>
         <h3 className="text-lg font-bold text-gray-800 uppercase ">
-        {subTitle}
+          {subTitle}
         </h3>
         <p className="mb-3 text-xs text-gray-700">{designation}</p>
         <div className="flex mb-1 text-sm text-gray-500">
@@ -66,7 +67,10 @@ function LargeUserCardWithDetails({
               <SocialIcon type="linkedin" url={socialLinks.linkedin} />
             )}
             {socialLinks.whatsapp && (
-              <SocialIcon type="whatsapp" url={`https://wa.me/+91${socialLinks.whatsapp}?text=Hello!%20I%20found%20your%20number%20from%20the%20Official%20Website%20of%20GPTC%20Perinthalmanna`} />
+              <SocialIcon
+                type="whatsapp"
+                url={`https://wa.me/+91${socialLinks.whatsapp}?text=Hello!%20I%20found%20your%20number%20from%20the%20Official%20Website%20of%20GPTC%20Perinthalmanna`}
+              />
             )}
           </div>
         )}
