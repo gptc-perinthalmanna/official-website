fe10fd7a10fc55a1a37e1ca4b6d1d4dc88ba1bf9
amjed-ali-k <amjedmgm@gmail.com>
2025-03-26 00:01:31 +0530

    fix: type error

--- components/custom/RibbonCard.tsx
+++ components/custom/RibbonCard.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import { useColor } from "../../common/hooks";
+import type React from "react";
+import { type ColorLiteral, useColor } from "../../common/hooks";
 
-function RibbonCard({ children, color = 'pink', ...props }: { children: React.ReactNode, color?: string, }) {
+function RibbonCard({ children, color = 'pink', ...props }: { children: React.ReactNode, color?: ColorLiteral, }) {
   const className = useColor(color);
   return (
-    <div className={"flex h-full border-2 border-t-0 border-b-0 border-l-8 border-r-0 border-pink-600 rounded-lg bg-gradient-to-r " + className} {...props}>
+    <div className={`flex h-full border-2 border-t-0 border-b-0 border-l-8 border-r-0 border-pink-600 rounded-lg bg-gradient-to-r ${className}`} {...props}>
       <div className="p-3">{children}</div>
     </div>
   );
